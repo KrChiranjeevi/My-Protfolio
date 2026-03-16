bdd8ebcccfcfaf8f3c9f55eb81da6032a9b967bf
Kumar Chiranjeevi <148898826+KrChiranjeevi@users.noreply.github.com>
2026-03-17 03:53:55 +0530

    fix: simplify App and update title for cache busting

--- src/App.jsx
+++ src/App.jsx
@@ -13,38 +13,28 @@ import Home from "./Pages/Home";
 import Portofolio from "./Pages/Portofolio";
 import WelcomeScreen from "./Pages/WelcomeScreen";
 import PropTypes from "prop-types";
-const LandingPage = ({ showWelcome, setShowWelcome }) => {
+const LandingPage = () => {
   return (
     <>
-      <AnimatePresence mode="wait">
-        {showWelcome && (
-          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
-        )}
-      </AnimatePresence>
-
-      {!showWelcome && (
-        <>
-          <Navbar />
-          <AnimatedBackground />
-          <FloatingParticles />
-          <Home />
-          <About />
-          <Portofolio />
-          <ContactPage />
-          <footer>
-            <center>
-              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  KR™
-                </a>
-                . All Rights Reserved.
-              </span>
-            </center>
-          </footer>
-        </>
-      )}
+      <Navbar />
+      <AnimatedBackground />
+      <FloatingParticles />
+      <Home />
+      <About />
+      <Portofolio />
+      <ContactPage />
+      <footer>
+        <center>
+          <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+          <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+            © 2025{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              KR™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </center>
+      </footer>
     </>
   );
 };
@@ -75,18 +65,15 @@ const ProjectPageLayout = () => (
 
 function App() {
   console.log("TRACE: App function execution");
-  const [showWelcome, setShowWelcome] = useState(true);
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
-          <Route path="/project/:id" element={<ProjectPageLayout />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/project/:id" element={<ProjectPageLayout />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
