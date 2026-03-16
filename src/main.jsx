355a984f097a345b7e00ebaa71164d1a8fd4cf83
Kumar Chiranjeevi <148898826+KrChiranjeevi@users.noreply.github.com>
2026-03-17 04:03:03 +0530

    debug: minimal render test to bypass App component

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ console.log("TRACE: main.jsx start");
 console.log("TRACE: root element:", document.getElementById("root"));
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
+// import App from "./App.jsx"
 import "./index.css"
-import ErrorBoundary from "./components/ErrorBoundary.jsx"
+// import ErrorBoundary from "./components/ErrorBoundary.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<ErrorBoundary>
-			<App />
-		</ErrorBoundary>
+    <div style={{ background: 'white', color: 'black', padding: '50px', fontSize: '3rem' }}>
+      HELLO WORLD - React is working!
+    </div>
 	</React.StrictMode>
 );
-console.log("TRACE: ReactDOM.render called");
+console.log("TRACE: Minimal Render called");
+锋
